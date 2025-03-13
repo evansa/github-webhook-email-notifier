ab0b54bc03b0342b1b6895b369e41b23c5154144
Evansa <fiveholiday55@hotmail.com>
2025-03-13 20:21:01 +0000

    Fix a few flake8 issues

--- app/config.py
+++ app/config.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+
 class Config:
     SECRET_TOKEN = os.getenv("GITHUB_SECRET_TOKEN")
     SMTP_SERVER = os.getenv("SMTP_SERVER")
@@ -11,5 +12,5 @@ class Config:
     SMTP_PASSWORD = os.getenv("SMTP_PASSWORD")
     EMAIL_TO = os.getenv("EMAIL_TO")
     EMAIL_FROM = os.getenv("EMAIL_FROM")
-
     PORT = int(os.getenv("PORT", 5000))
+    
